2b2bd41c56a8f754a82f553460f528faa9118fa8
Anjali Dass <anjali95912@yahoo.com>
2026-07-26 03:50:11 +0530

    Axios update

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 import type { VerifyApiResponse } from "../types/email";
 
+const API_URL =
+  import.meta.env.DEV
+    ? "http://localhost:5000"
+    : import.meta.env.VITE_API_URL;
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: API_URL,
 });
 
 export async function verifyEmail(email: string) {
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(
   cors({
     origin: [
+      "http://localhost:5173/",
       process.env.FRONTEND_URL,
     ],
     methods: ["GET", "POST"],

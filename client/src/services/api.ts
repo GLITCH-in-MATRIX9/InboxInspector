40a7f3da705ac744c010cb401f00bb9a9fc2f27f
Anjali Dass <anjali95912@yahoo.com>
2026-07-26 03:17:38 +0530

    Error Updates

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { VerifyApiResponse } from "../types/email";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export async function verifyEmail(email: string) {
